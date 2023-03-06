2f62f098d522b66d9797ef081b3e2dde97da610f
lvan-sav <vansav.ka@gmail.com>
2023-03-06 17:32:59 +0200

    Add login page

---
+++ test/pages/login.page.js
@@ -0,0 +1,55 @@
+import { Page } from "./page.js";
+
+
+const emailField = '//*[@content-desc="input-email"]'
+const passwordField = '//*[@content-desc="input-password"]'
+const loginBtn = '//*[@content-desc="button-LOGIN"]'
+const popUp = '//android.widget.FrameLayout'
+const successTitle = '//*[@resource-id="android:id/alertTitle"]'
+const successMsg = '//*[@resource-id="android:id/message"]'
+const okBtn = '//*[@resource-id="android:id/button1"]'
+
+class LoginPage extends Page {
+
+    async clickLoginBtn() {
+        await this.clickElem(loginBtn)
+    }
+
+    async clickOkBtn() {
+        await this.clickElem(okBtn)
+    }
+
+    async fillEmailField(text) {
+        await this.fillInField(emailField, text)
+    }
+
+    async fillPasswordField(text) {
+        await this.fillInField(passwordField, text)
+    }
+
+    async getSuccessTitle() {
+        return await this.getElem(successTitle)
+    }
+
+    async getSuccessMsg() {
+        return await this.getElem(successMsg)
+    }
+
+    async getSuccessTitleText() {
+        return await this.getElemText(successTitle)
+    }
+
+    async getSuccessMsgText() {
+        return await this.getElemText(successMsg)
+    }
+
+    async getOkBtn() {
+        return await this.getElem(okBtn)
+    }
+
+    async waitForPopUp() {
+        await $(popUp).waitForDisplayed({ timeout: 5000 })
+    }
+}
+
+export const loginPage = new LoginPage
